742f975f7a6831238f997b5d4480f47d139b2c06
Robin667 <robin.turpin8@gmail.com>
2023-01-16 22:43:40 +0100

    some css

--- moovue-app/src/components/MainHeader.vue
+++ moovue-app/src/components/MainHeader.vue
@@ -2,17 +2,17 @@
   <header
     class="flex flex-col sm:flex-row m-5 justify-between items-center text-center h-auto"
   >
-    <div class="flex flex-grow justify-evenly max-w-2xl">
+    <div class="flex flex-grow justify-evenly max-w-2xl mb-3">
       <router-link
         to="/"
-        class="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:dark:text-white"
+        class="flex flex-col items-center cursor-pointer group mr-4 sm:w-20 dark:text-gray-400 hover:dark:text-white"
       >
         <HomeIcon class="h-8 mb-1 group-hover:animate-bounce" />
         <p class="opacity-100 group-hover:opacity-100 tracking-widest">Home</p>
       </router-link>
       <router-link
         to="/list"
-        class="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:dark:text-white"
+        class="flex flex-col items-center cursor-pointer group mr-4 sm:w-20 dark:text-gray-400 hover:dark:text-white"
       >
         <EyeIcon class="h-8 mb-1 group-hover:animate-bounce" />
         <p class="opacity-100 group-hover:opacity-100 tracking-widest">
@@ -21,7 +21,7 @@
       </router-link>
       <router-link
         to="/likes"
-        class="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:dark:text-white"
+        class="flex flex-col items-center cursor-pointer group mr-4 sm:w-20 dark:text-gray-400 hover:dark:text-white"
       >
         <HeartIcon class="h-8 mb-1 group-hover:animate-bounce" />
         <p class="opacity-100 group-hover:opacity-100 tracking-widest">
@@ -30,20 +30,20 @@
       </router-link>
       <router-link
         to="/profil"
-        class="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:dark:text-white"
+        class="flex flex-col items-center cursor-pointer group sm:w-20 dark:text-gray-400 hover:dark:text-white"
       >
         <UserIcon class="h-8 mb-1 group-hover:animate-bounce" />
         <p class="opacity-100 group-hover:opacity-100 tracking-widest">
           Profil
         </p>
       </router-link>
-      <h1
-        class="text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl dark:text-white"
-      >
-        MooVue
-      </h1>
-      <DarkMode />
     </div>
+    <h1
+      class="text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl dark:text-white mb-3"
+    >
+      MooVue
+    </h1>
+    <DarkMode />
   </header>
 </template>
 
